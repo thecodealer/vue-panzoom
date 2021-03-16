c1872502a94446bd2e1d6a9ca188e0e5bad19cd8
Isa <mistabiggx@gmail.com>
2021-03-16 11:42:18 +0000

    Cleanup and dependency updates

--- dist/vue-panzoom.umd.js
+++ dist/vue-panzoom.umd.js
@@ -1,10 +1,10 @@
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('panzoom')) :
-    typeof define === 'function' && define.amd ? define(['panzoom'], factory) :
-    (global = global || self, global.VuePanZoom = factory(global.panZoom));
-}(this, function (panZoom) { 'use strict';
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('panzoom'), require('vue')) :
+    typeof define === 'function' && define.amd ? define(['panzoom', 'vue'], factory) :
+    (global = global || self, global.VuePanZoom = factory(global.panZoom, global.vue));
+}(this, (function (panZoom, vue) { 'use strict';
 
-    panZoom = panZoom && panZoom.hasOwnProperty('default') ? panZoom['default'] : panZoom;
+    panZoom = panZoom && Object.prototype.hasOwnProperty.call(panZoom, 'default') ? panZoom['default'] : panZoom;
 
     var PanZoomComponent = {
         name: 'panZoom',
@@ -106,144 +106,25 @@
         }
     };
 
-    function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
-    /* server only */
-    , shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
-      if (typeof shadowMode !== 'boolean') {
-        createInjectorSSR = createInjector;
-        createInjector = shadowMode;
-        shadowMode = false;
-      } // Vue.extend constructor export interop.
+    var _hoisted_1 = { class: "vue-pan-zoom-scene" };
 
-
-      var options = typeof script === 'function' ? script.options : script; // render functions
-
-      if (template && template.render) {
-        options.render = template.render;
-        options.staticRenderFns = template.staticRenderFns;
-        options._compiled = true; // functional template
-
-        if (isFunctionalTemplate) {
-          options.functional = true;
-        }
-      } // scopedId
-
-
-      if (scopeId) {
-        options._scopeId = scopeId;
-      }
-
-      var hook;
-
-      if (moduleIdentifier) {
-        // server build
-        hook = function hook(context) {
-          // 2.3 injection
-          context = context || // cached call
-          this.$vnode && this.$vnode.ssrContext || // stateful
-          this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext; // functional
-          // 2.2 with runInNewContext: true
-
-          if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
-            context = __VUE_SSR_CONTEXT__;
-          } // inject component styles
-
-
-          if (style) {
-            style.call(this, createInjectorSSR(context));
-          } // register component module identifier for async chunk inference
-
-
-          if (context && context._registeredComponents) {
-            context._registeredComponents.add(moduleIdentifier);
-          }
-        }; // used by ssr in case component is cached and beforeCreate
-        // never gets called
-
-
-        options._ssrRegister = hook;
-      } else if (style) {
-        hook = shadowMode ? function () {
-          style.call(this, createInjectorShadow(this.$root.$options.shadowRoot));
-        } : function (context) {
-          style.call(this, createInjector(context));
-        };
-      }
-
-      if (hook) {
-        if (options.functional) {
-          // register for functional component in vue file
-          var originalRender = options.render;
-
-          options.render = function renderWithStyleInjection(h, context) {
-            hook.call(context);
-            return originalRender(h, context);
-          };
-        } else {
-          // inject component registration as beforeCreate hook
-          var existing = options.beforeCreate;
-          options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
-        }
-      }
-
-      return script;
+    function render(_ctx, _cache, $props, $setup, $data, $options) {
+      return (vue.openBlock(), vue.createBlock("div", {
+        class: ["vue-pan-zoom-item", ['vue-pan-zoom-item-' + this.instanceId]]
+      }, [
+        vue.createVNode("div", _hoisted_1, [
+          vue.renderSlot(_ctx.$slots, "default")
+        ])
+      ], 2 /* CLASS */))
     }
 
-    var normalizeComponent_1 = normalizeComponent;
-
-    /* script */
-    var __vue_script__ = PanZoomComponent;
-
-    /* template */
-    var __vue_render__ = function() {
-      var _vm = this;
-      var _h = _vm.$createElement;
-      var _c = _vm._self._c || _h;
-      return _c(
-        "div",
-        {
-          staticClass: "vue-pan-zoom-item",
-          class: ["vue-pan-zoom-item-" + this.instanceId]
-        },
-        [_c("div", { staticClass: "vue-pan-zoom-scene" }, [_vm._t("default")], 2)]
-      )
-    };
-    var __vue_staticRenderFns__ = [];
-    __vue_render__._withStripped = true;
-
-      /* style */
-      var __vue_inject_styles__ = undefined;
-      /* scoped */
-      var __vue_scope_id__ = undefined;
-      /* module identifier */
-      var __vue_module_identifier__ = undefined;
-      /* functional template */
-      var __vue_is_functional_template__ = false;
-      /* style inject */
-      
-      /* style inject SSR */
-      
-      /* style inject shadow dom */
-      
-
-      
-      var __vue_component__ = normalizeComponent_1(
-        { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
-        __vue_inject_styles__,
-        __vue_script__,
-        __vue_scope_id__,
-        __vue_is_functional_template__,
-        __vue_module_identifier__,
-        false,
-        undefined,
-        undefined,
-        undefined
-      );
+    PanZoomComponent.render = render;
+    PanZoomComponent.__file = "src/components/pan-zoom/component.vue";
 
     var PanZoomPlugin = {
         install: function install(Vue, options) {
-            var _name = options && options.componentName ? options.componentName : __vue_component__.name;
-            Vue.component(_name, __vue_component__);
+            var _name = options && options.componentName ? options.componentName : PanZoomComponent.name;
+            Vue.component(_name, PanZoomComponent);
             Vue.prototype.$panZoom = panZoom;
         }
     };
@@ -254,4 +135,4 @@
 
     return PanZoomPlugin;
 
-}));
+})));
